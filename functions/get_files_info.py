2fb5ae8a1d7f6c5d431d690e3c70d19f637c58c6
josmanov <jusuf.osmanovic01@gmail.com>
2026-01-07 20:37:26 +0200

    print meta_data from files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,17 +3,28 @@ import os
 def get_files_info(working_directory, directory="."):
 
     working_dir_abs = os.path.abspath("calculator")
-    target_dir = os.path.normpoath(os.path.join(working_dir_abs, directory))
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
     if valid_target_dir == False:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        # Fix bug where directory content are being printed rather than printing only directory value
+        print(f"Error: Cannot list {directory} as it is outside the permitted working directory")
     if os.path.isdir(directory) == False:
         return f'Error: "{directory}" is not a directory'
 
+    target_dir_data = ""
     try:
-        for item in target_dir
-            target_dir_data += f'- {os.listdir(item)}: file_size={os.path.getsize(item)} bytes, is_dir={os.path.isdir(item)}\n'
-        return target_dir_data
+        if directory == '.':
+            target_dir_data = "Result for current directory:\n"
+        else:
+            target_dir_data = (f'Result for {directory} directory:\n')
+        try:
+            for file in os.listdir(target_dir):
+                file_size = os.path.getsize(target_dir + "/" + file)
+                is_dir = os.path.isdir(target_dir + "/" + file)
+                target_dir_data += (f'  - {file}: file_size={file_size} bytes, is_dir={is_dir}\n')
+        except Exception as e:
+            print(e)
     except:
         return f'Error: Could not fetch data from: {target_dir}'
-        
+
+    print(target_dir_data[:-1])
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,2 +1,8 @@
+from functions.get_files_info import get_files_info
+
 def main():
-    
+    result = get_files_info("calculator", ".")
+    print(result)
+
+if __name__ == "__main__":
+    main()
