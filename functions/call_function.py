d821489af389d66ecef37ba8ab519e853f10fb19
josmanov <jusuf.osmanovic01@gmail.com>
2026-01-13 20:30:07 +0200

    return output of function as types.Content

--- functions/call_function.py
+++ functions/call_function.py
@@ -39,3 +39,14 @@ def call_function(function_call, verbose=False):
         )
     args = dict(function_call.args) if function_call.args else {}
     args.update({"working_directory": "./calculator"})
+    function_result = function_map[function_name](**args)
+    
+    return types.Content(
+    role="tool",
+    parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result},
+        )
+    ],
+)
